719c9a57f0b32a4e987ef485bbe95ec70f2771a6
Your Name <dan@telent.net>
2010-02-25 12:59:40 +0000

    move the pending_command stuff into gui.c where it makes more sense

--- songlist.h
+++ songlist.h
@@ -12,7 +12,6 @@ struct song *read_songs(char *dirname) ;
 #ifndef IN_SONGLIST_C
 extern
 #endif
-struct command {
-    enum { NONE=0,SKIP,SEEK,PAUSE } tag;
-    int value;
-} pending_command;
+struct song *songs;
+
+
